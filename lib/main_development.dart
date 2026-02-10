5b9bc36fddea5d898f581ded3b14bad86db953d4
PauliCrespi <crespipauli@gmail.com>
2026-02-10 16:36:41 -0300

    feat: setup firebase ai

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,6 +1,14 @@
 import 'package:finance_app/app/app.dart';
 import 'package:finance_app/bootstrap.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_ai/firebase_ai.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  
   await bootstrap(() => const App());
 }
