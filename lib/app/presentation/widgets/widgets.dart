45bc8f4008848be39afa0c1b1282ca45d1bcaedb
Juan Rodríguez <79857303+juanRodriguez17@users.noreply.github.com>
2026-03-05 16:24:59 -0300

    feat: SparklineCard widget (#99)

--- lib/app/presentation/widgets/widgets.dart
+++ lib/app/presentation/widgets/widgets.dart
@@ -5,4 +5,5 @@ export 'horizontal_bar.dart';
 export 'metric_cards.dart';
 export 'radio_card.dart';
 export 'responsive_scaffold.dart';
+export 'sparkline_card.dart';
 export 'transaction_list.dart';
