dcef0f30dfed6dbe38db140bc06647ea98fe6a67
PauliCrespi <crespipauli@gmail.com>
2026-03-09 14:48:44 -0300

    test: added test for section header to design system catalog page test

--- test/dev_menu/view/design_system_catalog_page_test.dart
+++ test/dev_menu/view/design_system_catalog_page_test.dart
@@ -49,5 +49,31 @@ void main() {
 
       expect(find.byType(EmojiCardCatalogPage), findsOneWidget);
     });
+
+    testWidgets('renders SectionHeader list tile', (tester) async {
+      await _pumpPage(tester);
+
+      await tester.scrollUntilVisible(
+        find.text('SectionHeader'),
+        200,
+      );
+
+      expect(find.text('SectionHeader'), findsOneWidget);
+    });
+
+    testWidgets('tapping SectionHeader navigates to SectionHeaderCatalogPage', (
+      tester,
+    ) async {
+      await _pumpPage(tester);
+
+      await tester.scrollUntilVisible(
+        find.text('SectionHeader'),
+        200,
+      );
+      await tester.tap(find.text('SectionHeader'));
+      await tester.pumpAndSettle();
+
+      expect(find.byType(SectionHeaderCatalogPage), findsOneWidget);
+    });
   });
 }
