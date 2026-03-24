fefb4ee310f1e37f9a3d13caad0fa3e9857773f8
Brian Egan <brian.egan@verygood.ventures>
2026-03-24 12:10:42 +0000

    feat: Financial Advisor -> Life Goal Simulator

--- lib/onboarding/pick_profile/view/widgets/profile_card.dart
+++ lib/onboarding/pick_profile/view/widgets/profile_card.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:vgv_genui_financial_advisor/design_system/design_system.dart';
-import 'package:vgv_genui_financial_advisor/gen/assets.gen.dart';
-import 'package:vgv_genui_financial_advisor/l10n/l10n.dart';
-import 'package:vgv_genui_financial_advisor/onboarding/pick_profile/pick_profile.dart';
+import 'package:genui_life_goal_simulator/design_system/design_system.dart';
+import 'package:genui_life_goal_simulator/gen/assets.gen.dart';
+import 'package:genui_life_goal_simulator/l10n/l10n.dart';
+import 'package:genui_life_goal_simulator/onboarding/pick_profile/pick_profile.dart';
 
 class ProfileCard extends StatelessWidget {
   const ProfileCard({
