bc6c9bb0501b29c4f1dd5227dd9cdf4339094890
Brian Egan <brian.egan@verygood.ventures>
2026-03-24 14:14:24 +0000

    refactor: rename Financial Advisor to Life Goal Simulator (#229)
    * feat: Financial Advisor -> Life Goal Simulator
    
    * fix: final instances
    
    * fix: analysis errors

---
+++ lib/simulator/simulator.dart
@@ -0,0 +1,7 @@
+export 'bloc/bloc.dart';
+export 'catalog/catalog.dart';
+export 'prompt/prompt.dart';
+export 'repository/simulator_repository.dart';
+export 'view/simulator_page.dart';
+export 'view/simulator_view.dart';
+export 'view/widgets/simulator_message_bubble.dart';
