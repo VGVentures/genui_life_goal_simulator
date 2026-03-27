f1756adb03b5c0773ee69d9ad8a2f43152d5e9e9
Noe Fernandez <91338515+noe-xceptions@users.noreply.github.com>
2026-03-27 16:40:08 -0300

    feat: add InsightCard component and catalog item for contextual insights

--- lib/design_system/widgets/widgets.dart
+++ lib/design_system/widgets/widgets.dart
@@ -10,6 +10,7 @@ export 'filter_bar.dart';
 export 'gcn_slider.dart';
 export 'header_selector.dart';
 export 'horizontal_bar.dart';
+export 'insight_card.dart';
 export 'line_chart.dart';
 export 'metric_cards.dart';
 export 'pie_chart/widgets.dart';
